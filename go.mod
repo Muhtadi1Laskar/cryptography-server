5ebf6428c558b25f16b29467d88b64420576de1e
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-20 05:44:00 +0600

    Added server

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module cryptographyServer
 go 1.23.0
 
 require (
+	github.com/gorilla/mux v1.8.1 // indirect
 	golang.org/x/crypto v0.29.0 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"cryptographyServer/hashs"
+	"encoding/json"
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+func hashData (w http.ResponseWriter, r *http.Request) {
+	h, _ := hashs.Hash("Hello World", "sha256")
+
+	w.Header().Set("Content-Type", "Application/json")
+	json.NewEncoder(w).Encode(h)
+}
+
+
 func main() {
-	var message string = "Hello World"
+	router := mux.NewRouter()
+	router.HandleFunc("/hashData", hashData).Methods("GET")
 
-	hashedMsg, err := hashs.Hash(message, "blake2b_512")
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	fmt.Println("Hashed Message: ", hashedMsg)
+	fmt.Printf("Server running on port 5000\n")
+	http.ListenAndServe(":5000", router)
 }
